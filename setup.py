fb98e2ae9d57e1f5d524998473bf1e0a4bdf6b43
Steven Cummings <steven@cummingsbrothers.net>
2011-08-07 23:28:30 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+
+# http://packages.python.org/distribute/setuptools.html#using-setuptools-without-bundling-it
+import distribute_setup
+distribute_setup.use_setuptools()
+
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+    name='pythonkc-meetups',
+    version='0.0.1',
+    description='Provides PythonKC Meetup.com events.',
+    license='MIT',
+    url='https://github.com/estebistec/pythonkc-meetups',
+    packages=find_packages(),
+    install_requires=['distribute>=0.6.14', 'httplib2>=0.7.1', 
+                      'mimeparse>=0.1.3', 'python-dateutil>=1.5,<2'],
+    author='Steven Cummings',
+    author_email='estebistec@gmail.com'
+)
