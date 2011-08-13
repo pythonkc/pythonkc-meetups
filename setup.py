29a0398fbe9072e8b6cd49f6f0069abf7beeb274
Steven Cummings <estebistec@gmail.com>
2011-08-13 18:11:39 -0500

    Updated project website, added pypi classifiers

--- setup.py
+++ setup.py
@@ -10,10 +10,19 @@ setup(
     version='0.0.2',
     description='Provides PythonKC Meetup.com events.',
     license='MIT',
-    url='https://github.com/estebistec/pythonkc-meetups',
+    url='https://github.com/pythonkc/pythonkc-meetups',
     packages=find_packages(),
     install_requires=['distribute>=0.6.14', 'httplib2>=0.7.1', 
                       'mimeparse>=0.1.3', 'python-dateutil>=1.5,<2'],
     author='Steven Cummings',
-    author_email='estebistec@gmail.com'
+    author_email='estebistec@gmail.com',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries'
+    ]
 )
